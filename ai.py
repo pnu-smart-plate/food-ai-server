bac6b35447a994be030763b395c79049c44e274f
student-p <paulmoon00@naver.com>
2024-09-15 22:26:32 +0900

    feat: 식판 인식모델 추가

--- ai.py
+++ ai.py
@@ -3,7 +3,15 @@ from ultralytics import YOLO
 
 class DetectionModel:
     def __init__(self):
-        self.model = YOLO("best.pt")
+        self.model = YOLO("food-detection.pt")
+
+    def predict(self, img):
+        return self.model.predict(img)
+
+
+class TrayDetectionModel:
+    def __init__(self):
+        self.model = YOLO("tray-detection.pt")
 
     def predict(self, img):
         return self.model.predict(img)
